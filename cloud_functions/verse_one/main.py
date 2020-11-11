1712f20abf116a4b934c25aee300a0aa3dcb6dc2
worldofchris <chris@fooengine.com>
2020-11-11 15:09:55 +0000

    Added more activity to first verse

--- cloud_functions/verse_one/main.py
+++ cloud_functions/verse_one/main.py
@@ -28,22 +28,24 @@ def get_auth_token(endpoint):
 
 def entry_point(event, context):
 
-    with tracer.start_as_current_span(name="verse_one") as span:
+    with tracer.start_as_current_span(name="sing_verse_one") as span:
 
         lyric = "Don't you ever, don't you ever"
         logger.info(lyric)
         span.add_event(lyric)
 
-        chorus_endpoint = f'https://{environ["LOCATION"]}-{environ["GCP_PROJECT_ID"]}.cloudfunctions.net/{environ["CHORUS_FUNCTION"]}'
-        auth_token = get_auth_token(chorus_endpoint)
-        function_headers = {'Authorization': f'bearer {auth_token}', 'content-type': 'application/json'}
-        trace_headers = bm.get_traceparent_header(span)
+        with tracer.start_as_current_span(name="sing_chorus") as span:
 
-        request = requests.get(chorus_endpoint,
-                               headers={**function_headers, **trace_headers})
-        if request.status_code != 200:
-            logger.error(f'{chorus_endpoint} returned:{request.status_code}')
-            return
+            chorus_endpoint = f'https://{environ["LOCATION"]}-{environ["GCP_PROJECT_ID"]}.cloudfunctions.net/{environ["CHORUS_FUNCTION"]}'
+            auth_token = get_auth_token(chorus_endpoint)
+            function_headers = {'Authorization': f'bearer {auth_token}', 'content-type': 'application/json'}
+            trace_headers = bm.get_traceparent_header(span)
+
+            request = requests.get(chorus_endpoint,
+                                   headers={**function_headers, **trace_headers})
+            if request.status_code != 200:
+                logger.error(f'{chorus_endpoint} returned:{request.status_code}')
+                return
 
         publisher = pubsub_v1.PublisherClient()
         topic_path = publisher.topic_path(environ['GCP_PROJECT_ID'],
